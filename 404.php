f99c39e8f1f30f394385ec078132956b1d541498
oscaradiaz10 <oscar.di@me.com>
2024-11-04 15:26:53 -0600

    Set up 404 page

---
+++ 404.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Not Found</title>
+
+    <?php include 'sub/links.php'; ?>
+
+</head>
+
+<body>
+
+<?php include 'sub/main-menu.php'; ?>
+
+<div class="container">
+    <div class="row" style="text-align: center;">
+        <div class="col-12">
+            <h1 style="font-size: 6rem;">404</h1>
+            <h3>Page not found!</h3>
+            <hr>
+            <p>Sorry, we couldn't find the page you're looking for.</p>
+            <button type="button" class="btn btn-primary">
+                <a href="index.php">Return Home</a>
+            </button>
+        </div>
+    </div>
+</div>
+
+<?php include 'sub/components/call-to-action-jumbotron.php'; ?>
+
+<?php include 'sub/footer.php'; ?>
+
+</body>
+</html>
